962e5734bb557374f4db15a8fdebee87154bf76e
Soorria Saruva <soorria.ss@gmail.com>
2022-04-05 10:05:33 +1000

    add vite inspect plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { join } from 'path'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [inspect(), solidPlugin()],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
