a143c107b07dac7125213fc368b74cb132a91198
Soorria Saruva <soorria.ss@gmail.com>
2022-04-22 15:50:37 +1000

    fix bug

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -12,8 +12,8 @@ const E = 0.001
 const getColorForDayScore = (s: string | number): string => {
   if (s === 'X') return scoreGoodnessTextColors.bad
   if (typeof s === 'string') return ''
-  if (Math.round(s) < 3) return scoreGoodnessTextColors.good
-  if (Math.round(s) < 5) return scoreGoodnessTextColors.ok
+  if (Math.round(s) <= 3) return scoreGoodnessTextColors.good
+  if (Math.round(s) <= 5) return scoreGoodnessTextColors.ok
   return scoreGoodnessTextColors.bad
 }
 
