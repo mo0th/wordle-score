1d124048ec0dd4cc5fc9a9d099987f0a50b7f848
Soorria Saruva <soorria.ss@gmail.com>
2022-04-24 18:16:41 +1000

    minify html

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { join } from 'path'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import inspect from 'vite-plugin-inspect'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-  plugins: [inspect(), solidPlugin()],
+  plugins: [
+    inspect(),
+    solidPlugin(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
