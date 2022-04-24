2dab1739a28cd239e2353033bda10b297c05b4f8
Soorria Saruva <soorria.ss@gmail.com>
2022-04-24 19:49:05 +1000

    reduce padding on sandbox indicator

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ const App: Component = () => {
       <Confetti />
       <div class="flex h-full flex-col">
         <Show when={settings.sandboxMode}>
-          <div class="fixed inset-x-0 top-0 z-40 bg-yellow-300 p-2 font-bold text-black">
+          <div class="fixed inset-x-0 top-0 z-40 bg-yellow-300 py-2 font-bold text-black">
             <Container>Sandbox mode enabled - changes won't be saved</Container>
           </div>
         </Show>
