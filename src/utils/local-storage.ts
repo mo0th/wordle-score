17068757d3ac311530f06c431675e135b71b72ce
Soorria Saruva <soorria.ss@gmail.com>
2022-05-04 12:39:34 +1000

    add forgotten file

---
+++ src/utils/local-storage.ts
@@ -0,0 +1,43 @@
+import { Accessor, createEffect, createSignal, Setter } from 'solid-js'
+import { createStore, SetStoreFunction, Store } from 'solid-js/store'
+import { shouldSync } from '~/lib/should-sync'
+
+type Validate<T> = (obj: unknown) => obj is T
+
+const getInitialValue = <T>(key: string, fallback: T, validate?: Validate<T>): T => {
+  try {
+    const cachedString = localStorage.getItem(key)
+    if (!cachedString) return fallback
+    const parsed = JSON.parse(cachedString)
+    if (validate && !validate(parsed)) {
+      return fallback
+    }
+    return parsed
+  } catch (err) {
+    return fallback
+  }
+}
+
+export const createLocalSignal = <T>(
+  key: string,
+  initial: T,
+  validate?: Validate<T>
+): [Accessor<T>, Setter<T>] => {
+  const [state, setState] = createSignal<T>(getInitialValue(key, initial, validate))
+  createEffect(() => {
+    if (shouldSync()) localStorage.setItem(key, JSON.stringify(state()))
+  })
+  return [state, setState]
+}
+
+export const createLocalStore = <T>(
+  key: string,
+  initial: T,
+  validate?: Validate<T>
+): [Store<T>, SetStoreFunction<T>] => {
+  const [store, setStore] = createStore<T>(getInitialValue(key, initial, validate))
+  createEffect(() => {
+    if (shouldSync()) localStorage.setItem(key, JSON.stringify(store))
+  })
+  return [store, setStore]
+}
